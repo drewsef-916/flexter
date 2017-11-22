e17e09759bb33051a64968af2ce6f92959a4610e
drewsef-916 <drewm541@gmail.com>
2017-11-22 01:00:45 +0000

    challenge 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def current_course
+    @current_course ||= current_lesson.section.course
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user && ! current_lesson.section.course.user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You must be enrolled in the course to view this content!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
